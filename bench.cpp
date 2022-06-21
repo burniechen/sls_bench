78ef5ab745906a943f776b10ba0c801495258cc4
burniechen <chen0317029@gmail.com>
2022-06-21 11:18:32 +0800

    [FILE] bench.cpp test all tables

--- bench.cpp
+++ bench.cpp
@@ -1,58 +1,86 @@
 #include <iostream> 
 #include <fstream>
+#include <string>
+#include <filesystem>
+#include <vector>
 
 #include "src/sls.hpp"
 #include "src/bench.hpp"
 
 using namespace std;
+namespace fs = std::filesystem;
 
 int main() {
-	auto rnd = 4;
-	auto power = 7;
-	ofstream fout("rmc3.csv");
+	auto rnd = 1;
+	auto power = 1;
+	ofstream fout("rmc1.csv");
+	bool fout_flag = false;
+
+	string path = "/home/nctu/dlrm-file/dlrm/table_rm1/";
+	auto sum = vector<vector<double>> (power, vector<double> (4, 0));
 
 	for (auto i=0; i<power; ++i) {
-		sls_config *config = new sls_config("table/rmc3", 2000000, 32, 20, 1<<i);
+		for (auto it : fs::directory_iterator(path)) {
+			string emb = fs::absolute(it);
+			sls_config *config = new sls_config(emb, 4000000, 32, 80, 1<<i);
 
-		auto test_io_buf = bind(sls_io_buf, config);
-		auto pre_io_buf = bind(pre_hook, config, "io_buf");
-		auto post_io_buf = bind(post_hook, config, "io_buf");
+			auto test_io_buf = bind(sls_io_buf, config);
+			auto pre_io_buf = bind(pre_hook, config, "io_buf"); 
+			auto post_io_buf = bind(post_hook, config, "io_buf");
 
-		auto test_io_unbuf = bind(sls_io_unbuf, config);
-		auto pre_io_unbuf = bind(pre_hook, config, "io_unbuf");
-		auto post_io_unbuf = bind(post_hook, config, "io_unbuf");
+			auto test_io_unbuf = bind(sls_io_unbuf, config);
+			auto pre_io_unbuf = bind(pre_hook, config, "io_unbuf");
+			auto post_io_unbuf = bind(post_hook, config, "io_unbuf");
 
-		auto test_mmap = bind(sls_mmap, config);
-		auto pre_mmap = bind(pre_hook, config, "mmap");
-		auto post_mmap = bind(post_hook, config, "mmap");
+			auto test_mmap = bind(sls_mmap, config);
+			auto pre_mmap = bind(pre_hook, config, "mmap");
+			auto post_mmap = bind(post_hook, config, "mmap");
 
-		auto test_ram = bind(sls_ram, config);
-		auto pre_ram = bind(pre_hook, config, "ram");
-		auto post_ram = bind(post_hook, config, "ram");
+			auto test_ram = bind(sls_ram, config);
+			auto pre_ram = bind(pre_hook, config, "ram");
+			auto post_ram = bind(post_hook, config, "ram");
 
-		auto bench_io_buf = bm::real_time(test_io_buf, pre_io_buf, post_io_buf);
-		auto bench_io_unbuf = bm::real_time(test_io_unbuf, pre_io_unbuf, post_io_unbuf);
-		auto bench_mmap = bm::real_time(test_mmap, pre_mmap, post_mmap);
-		auto bench_ram = bm::real_time(test_ram, pre_ram, post_ram);
+			auto bench_io_buf = bm::real_time(test_io_buf, pre_io_buf, post_io_buf);
+			auto bench_io_unbuf = bm::real_time(test_io_unbuf, pre_io_unbuf, post_io_unbuf);
+			auto bench_mmap = bm::real_time(test_mmap, pre_mmap, post_mmap);
+			auto bench_ram = bm::real_time(test_ram, pre_ram, post_ram);
 
-		auto result = bm::bench(rnd, bm::excl_avg<bm::nanos, 1>,
-								bench_io_buf, 
-								bench_io_unbuf,
-								bench_mmap,
-								bench_ram);
+			auto result = bm::bench(rnd, bm::excl_avg<bm::nanos, 1>,
+									bench_io_buf, 
+									bench_io_unbuf,
+									bench_mmap,
+									bench_ram);
 
-		cout << "[TIME]\n";
-		fout << (1<<i) << ',';
-		for (auto e : result) {
-			cout << (1<<i) << " : " << e.count()/1000 << endl;
-			fout << e.count() << ',';
+			cout << "[Time]\n";
+			auto type = 0ul;
+			for (auto e : result) {
+				printf("(%s, %d): %lu\n", emb.c_str(), (1<<i), e.count()/1000);
+				sum[i][type] += (e.count()/1000);
+				type++;
+			}
+			cout << endl;
+
+			delete config;
 		}
-		cout << endl;
-		fout << endl;
-
-		delete config;
-		fout.close();
 	}
+
+	cout << "[Break down]\n";
+	for (auto r : sum) {
+		for (auto e : r)
+			cout << e << ' ';
+		cout << endl;
+	}
+
+	if (fout_flag) {
+		for (auto i=0; i<power; ++i) {
+			fout << (1<<i) << ',';
+			for (auto j=0; j<4; ++j)
+				fout << sum[i][j] << ',';
+			fout << endl;
+		}
+	}
+
+	fout.close();
 
 	return 0;
 }
--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+make clean
+make
 ASAN_OPTIONS=detect_leaks=1 ./bench
