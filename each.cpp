b30daf040ee0f8bf81dfcc16e569c58e8351e1ec
burniechen <chen0317029@gmail.com>
2022-06-22 00:57:34 +0800

    [FILE] each.cpp add func. ptr

--- each.cpp
+++ each.cpp
@@ -43,20 +43,29 @@ int main(int argc, char *argv[]) {
 	auto C = stoi(arg["--feature-size"]);
 	// auto K = stoi(arg["--lengths-count"]); // batch-size
 	auto L = stoi(arg["--num-indices-per-lookup"]);
+	auto type = arg["--type"];
+
+	void (*fun)(sls_config*);
+	if (type == "io_buf") fun = &sls_io_buf;
+	if (type == "io_unbuf") fun = &sls_io_unbuf;
+	if (type == "mmap") fun = &sls_mmap;
+	if (type == "ram") fun = &sls_ram;
 
 	auto sum = 0;
 	for (auto i=0; i<power; ++i) {
 		for (auto it : fs::directory_iterator(dir)) {
-			string emb = fs::absolute(it);
+			auto emb = fs::absolute(it);
 			sls_config *config = new sls_config(emb, R, C, 1<<i, L);
 
-			auto test_ram = bind(sls_ram, config);
-			auto pre_ram = bind(pre_hook, config, "ram");
-			auto post_ram = bind(post_hook, config, "ram");
+			auto test_fun = bind(*fun, config);
+			auto pre_fun = bind(pre_hook, config, type);
+			auto post_fun = bind(post_hook, config, type);
 
-			auto bench_ram = bm::real_time(test_ram, pre_ram, post_ram);
+			auto bench_fun = bm::real_time(test_fun, 
+											pre_fun, 
+											post_fun);
 
-			auto result = bm::bench(rnd, bm::excl_avg<bm::nanos, 1>, bench_ram);
+			auto result = bm::bench(rnd, bm::excl_avg<bm::nanos, 1>, bench_fun);
 			sum += (result.count()/1000);
 
 			cout << "[Time]\n";
